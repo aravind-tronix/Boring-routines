59f620f6a86f66179a981603a4f0c2dc0b639b61
Aravind Prabaharan <aravindabilash6@gmail.com>
2022-01-09 10:58:05 +0530

    initial commit

--- client-asshole.py
+++ client-asshole.py
@@ -15,51 +15,50 @@ import time
 
 
 def incommingmail():
-	#credentials
-	username ="xxxxxx@gmail.com"        #your email
+    #credentials
+    username ="xxxxxx@gmail.com"        #your email
 
-	# app password
-	app_password= "xxxxxxx"             #password
+    # app password
+    app_password= "xxxxxxx"             #password
 
-	# For more >> https://www.systoolsgroup.com/imap/
-	gmail_host= 'imap.gmail.com'        #gmail host
+    # For more >> https://www.systoolsgroup.com/imap/
+    gmail_host= 'imap.gmail.com'        #gmail host
 
-	#set connection
-	mail = imaplib.IMAP4_SSL(gmail_host)
+    #set connection
+    mail = imaplib.IMAP4_SSL(gmail_host)
 
-	#login
-	log = mail.login(username, app_password)
-	print(log)
+    #login
+    log = mail.login(username, app_password)
+    print(log)
 
-	#select inbox
-	mail.select("INBOX")
-	n=0
-	#select specific mails from specific user
-	retcode, selected_mails = mail.search(None,'(UNSEEN)', '(FROM "xxxxxxx.com")') #reads unread email from specified email id(say clients)
-	if retcode == 'OK':
-		for num in selected_mails[0].split() :
-		  #print(num)
-		  print ('Processing ')
-		  n=n+1
-		  typ, data = mail.fetch(num,'(RFC822)')
-		  for response_part in data:
-			if isinstance(response_part, tuple):
-				original = email.message_from_bytes(data[0][1])
-				print (original['From'])
-				Subject = (original['Subject'])
-				for part in original.walk():
-					if part.get_content_type()=="text/plain" or part.get_content_type()=="text/html":
-						message = part.get_payload(decode=True)
-						m = (message.decode())
-				if  (Subject.find(('help') or ('trouble') or ('sorry'))!=-1) or (m.find(('help') or ('trouble') or ('sorry'))!=-1):
-						sshconnection()
-						sendmail()
-						typ, data = mail.store(num,'+FLAGS','\\Seen')
-				else:
-					typ, data = mail.store(num,'-FLAGS','\\Seen')
-			
-
+    #select inbox
+    mail.select("INBOX")
+    n=0
+    #select specific mails from specific user
+    retcode, selected_mails = mail.search(None,'(UNSEEN)', '(FROM "xxxxxxx.com")') #reads unread email from specified email id(say clients)
+    if retcode == 'OK':
+        for num in selected_mails[0].split() :
+          #print(num)
+          print ('Processing ')
+          n=n+1
+          typ, data = mail.fetch(num,'(RFC822)')
+          for response_part in data:
+            if isinstance(response_part, tuple):
+                original = email.message_from_bytes(data[0][1])
+                print (original['From'])
+                Subject = (original['Subject'])
+                for part in original.walk():
+                    if part.get_content_type()=="text/plain" or part.get_content_type()=="text/html":
+                        message = part.get_payload(decode=True)
+                        m = (message.decode())
+                if  (Subject.find(('help') or ('trouble') or ('sorry'))!=-1) or (m.find(('help') or ('trouble') or ('sorry'))!=-1):
+                    sshconnection()
+                    sendmail()
+                    typ, data = mail.store(num,'+FLAGS','\\Seen')
+                else:
+                    typ, data = mail.store(num,'-FLAGS','\\Seen')
+            
 schedule.every(10).minutes.do(incommingmail)     #check for incomming email every 10 minutes
 while True: 
-	schedule.run_pending() 
-	time.sleep(1) 
+  schedule.run_pending() 
+  time.sleep(1) 
